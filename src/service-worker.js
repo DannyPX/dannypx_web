0646b86b1dc77aaf27c6a3f83d5cf4b13578d39a
DannyPX <Damnoga@hotmail.com>
2020-06-11 17:54:07 +0200

    service-worker importScript

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,7 @@
-import { skipWaiting, clientsClaim } from "workbox-core";
-import { precacheAndRoute } from "workbox-precaching";
+/* eslint-disable no-console */
+importScripts("/precache-manifest.819d51f67fda03f9f8513caa65ca9c4b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-skipWaiting();
-clientsClaim();
-precacheAndRoute(self.__precacheManifest);
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
